d74c97badd84fb4ef39a99c5ea1d36286f6ee4a2
Romanimuss <88721472+Romanimuss@users.noreply.github.com>
2021-11-10 12:09:06 +0100

    Restart For htdocs

---
+++ day3.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+       <title></title>
+   </head>
+   <body>
+       <?php
+       $a = "Kefaet";
+       //$b = 0;
+       for( $i=0; $i<5; $i++)
+       {
+           $a += 10;
+           //$b += 5 ;
+       }
+       echo ("At the end of the loop a=$a");
+       // Output:
+       // The loop will Print a=Kefaet on the Screen
+       ?>
+   </body>
+</html>
