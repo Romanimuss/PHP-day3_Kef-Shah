8539f4e58517bd803655d77379187ffad4b42b20
Romanimuss <88721472+Romanimuss@users.noreply.github.com>
2021-11-10 12:33:23 +0100

    just to have it on the screen

--- day3.php
+++ day3.php
@@ -4,15 +4,15 @@
        <meta charset="UTF-8">
        <meta name="viewport" content="width=device-width, initial-scale=1.0">
        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-       <title></title>
+       <title>Kefaet&Shahpar</title>
    </head>
    <body>
        <?php
        $a = "Kefaet";
        //$b = 0;
-       for( $i=0; $i<5; $i++)
+       for( $i=0; $i<50;)
        {
-           $a += 10;
+           $a += "Kefaet";
            //$b += 5 ;
        }
        echo ("At the end of the loop a=$a");
